deb67deff8d80cc06e4905bac54663e86d2d404d
KDN <kdn@gcsc.co.kr>
2025-06-24 14:16:36 +0900

    [pubilsh] style: 스타일 업데이트

---
+++ src/publish/mainPage.tsx
@@ -0,0 +1,11 @@
+import "@/assets/scss/user-styles.scss";
+
+const MainPage = () => {
+  return (
+    <div className="main-page">
+      <h1>main</h1>
+    </div>
+  );
+};
+
+export default MainPage;
