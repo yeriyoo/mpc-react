911edeae11d2fcca3df0c216679116befb41316d
yeriyoo <ylyoo@gcsc.co.kr>
2025-10-20 10:31:59 +0900

    Update 10/20

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { BrowserRouter as Router } from 'react-router-dom';
-import Publish from './Publish';
-function App() {
-    return (_jsx(Router, { children: _jsx(Publish, {}) }));
-}
-export default App;
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.min.css";
-import Publish from './Publish';
-import MainPage from './pages/MainPage';
-import LoginMain from './pages/LoginMain';
-createRoot(document.getElementById('root')).render(_jsx(StrictMode, { children: _jsx(HashRouter, { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Publish, {}) }), _jsx(Route, { path: "/LoginMain", element: _jsx(LoginMain, {}) }), _jsx(Route, { path: "/MainPage", element: _jsx(MainPage, {}) }), _jsx(Route, { path: "/MainPage/:menuId", element: _jsx(MainPage, {}) })] }) }) }));
