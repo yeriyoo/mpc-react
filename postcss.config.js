6b51b38a61d5e9a837de3cc29455b1ae8ab273dd
yeriyoo <ylyoo@gcsc.co.kr>
2025-10-27 12:49:32 +0900

    10/27

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,12 @@
 // postcss.config.js
-module.exports = {
-  plugins: [
-    require('autoprefixer')({
-      overrideBrowserslist: ['>0.2%', 'not dead', 'not op_mini all'],
-      flexbox: 'no-2009', 
-      grid: 'autoplace', 
-    }),
-  ],
+export default {
+  plugins: {
+    autoprefixer: {
+      overrideBrowserslist: [
+        ">0.2%",
+        "not dead",
+        "not op_mini all"
+      ],
+    },
+  },
 };
